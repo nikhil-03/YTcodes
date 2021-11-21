8a74ae548bd2973d9894b19668022724de8ea893
Nikhil <nikhilprakash2012@gmail.com>
2021-11-21 14:28:00 +0530

    21-11-2021

---
+++ datatypes.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	// int a;
+	// a=15;
+
+
+    // char a='c';
+
+    // float a;
+
+    // a=5.2;
+
+    // cout<<a*6<<endl;
+
+    
+    // bool a=0;
+
+ //    string s;
+
+ //    cin>>s;
+
+	// cout<<s;
+	// int a;
+	// cout<<INT_MAX;
+	long long int a=15;
+	cout<<LLONG_MAX;
+
+}
